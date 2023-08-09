32d38882ef9cd5ca00592b10ce9f3aa96fefee76
errasti13 <jon44errasti@gmail.com>
2023-08-09 17:55:23 +0200

    Plot Styles

--- LeyBiotSavart3D.m
+++ LeyBiotSavart3D.m
@@ -1,46 +1,57 @@
-%% GRUPO 13 Alonso Lucas, Sara; Errasti Odriozola, Jon; 
-%%          Sarabia Vargas, Alejandro; Terreros Sanchez, Carlos
-%           Función para la aplicacion de la Ley de Biot-Savart
+% calculateVortexInfluence - Calculate the velocity components induced by a vortex segment
+%
+% Syntax:
+%   [Velocidad_z, Velocidad_y, Velocidad_x] = calculateVortexInfluence(xr, yr, zr, xP, yP, zP, xQ, yQ, zQ)
+%
+% Inputs:
+%   xr, yr, zr - Coordinates of the point where influence velocity is calculated
+%   xP, yP, zP - Coordinates of the vortex segment start point
+%   xQ, yQ, zQ - Coordinates of the vortex segment end point
+%
+% Outputs:
+%   Velocidad_z - Influence velocity in the z direction at the specified point
+%   Velocidad_y - Influence velocity in the y direction at the specified point
+%   Velocidad_x - Influence velocity in the x direction at the specified point
+%
+% Description:
+%   This function calculates the velocity components induced by a vortex
+%   segment formed by points P and Q on a given point (xr, yr, zr) using
+%   the Biot-Savart law. The resulting velocities are provided in the z, y,
+%   and x directions.
+%
+% Example:
+%   % Calculate the influence velocity components at a point due to a vortex segment
+%   [Velocidad_z, Velocidad_y, Velocidad_x] = calculateVortexInfluence(xr, yr, zr, xP, yP, zP, xQ, yQ, zQ);
+%
+% Authors: Alonso Lucas, Sara; Errasti Odriozola, Jon;
+%          Sarabia Vargas, Alejandro; Terreros Sanchez, Carlos
 
 
-function [Velocidad_z,Velocidad_y,Velocidad_x]=LeyBiotSavart3D(xr,yr,zr,xP,yP,zP,xQ,yQ,zQ)
-
-% Esta funcion calcula el factor de influencia 'w' que un hilo de torbellinos
-% formado por un segmento 'PQ' genera en un punto arbitrario 'r'.
-
-% Una vez enunciada la Ley de Biot-Savart y establecer las distintas
-% relaciones para llegar a la formula final, definimos los modulos de los
-% vectores que se utilizaran para calcular el factor de influencia:
-% vector rrP y vector rrQ.
-
-modulo_rrP=sqrt((xr-xP).^2 +(yr-yP).^2+(zr-zP).^2);
-modulo_rrQ=sqrt((xr-xQ).^2 +(yr-yQ).^2+(zr-zQ).^2);
-
-% Se procedera a calcular a continuacion los productos escalares
-% necesarios para la obtencion del factor de influencia: rPQ . rrP y rPQ .
-% rrQ.
-
-prod_escalar_rPQrrP= (xQ-xP).*(xr-xP)+(yQ-yP).*(yr-yP)+(zQ-zP).*(zr-zP);
-prod_escalar_rPQrrQ= (xQ-xP).*(xr-xQ)+(yQ-yP).*(yr-yQ)+(zQ-zP).*(zr-zQ);
-
-% El siguiente paso sera calcular el producto vectorial entre rrP y rrQ y
-% su respectivo modulo; como z=0, solo habra componente en z del producto
-% vectorial.
-
-rrPxrrQ_z=(xr-xP).*(yr-yQ)-(xr-xQ).*(yr-yP); %Componente z
-rrPxrrQ_x=(zr-zP).*(yr-yQ)-(zr-zQ).*(yr-yP); %Componente x
-rrPxrrQ_y=(xr-xP).*(zr-zQ)-(xr-xQ).*(zr-zP); %Componente y
-
-% Por ultimo, se tiene todo lo necesario para obtener el factor de
-% influencia.
-
-modulo_rrPxrrQ_2=(rrPxrrQ_z.^2+rrPxrrQ_x.^2+rrPxrrQ_y.^2);
-
-Velocidad_z=1/4/pi.*rrPxrrQ_z./(modulo_rrPxrrQ_2+1e-12).*...
-(prod_escalar_rPQrrP./modulo_rrP-prod_escalar_rPQrrQ./modulo_rrQ);
-Velocidad_y=1/4/pi.*rrPxrrQ_y./(modulo_rrPxrrQ_2+1e-12).*...
-(prod_escalar_rPQrrP./modulo_rrP-prod_escalar_rPQrrQ./modulo_rrQ);
-Velocidad_x=1/4/pi.*rrPxrrQ_x./(modulo_rrPxrrQ_2+1e-12).*...
-(prod_escalar_rPQrrP./modulo_rrP-prod_escalar_rPQrrQ./modulo_rrQ);
-
+function [Velocidad_z, Velocidad_y, Velocidad_x] = calculateVortexInfluence(xr, yr, zr, xP, yP, zP, xQ, yQ, zQ)
+    % Calculate vortex influence velocity components on a point
+    
+    % Calculate distances
+    modulo_rrP = sqrt((xr - xP).^2 + (yr - yP).^2 + (zr - zP).^2);
+    modulo_rrQ = sqrt((xr - xQ).^2 + (yr - yQ).^2 + (zr - zQ).^2);
+    
+    % Calculate scalar products
+    prod_escalar_rPQrrP = (xQ - xP) .* (xr - xP) + (yQ - yP) .* (yr - yP) + (zQ - zP) .* (zr - zP);
+    prod_escalar_rPQrrQ = (xQ - xP) .* (xr - xQ) + (yQ - yP) .* (yr - yQ) + (zQ - zP) .* (zr - zQ);
+    
+    % Calculate vector cross product components
+    rrPxrrQ_z = (xr - xP) .* (yr - yQ) - (xr - xQ) .* (yr - yP); % Component z
+    rrPxrrQ_x = (zr - zP) .* (yr - yQ) - (zr - zQ) .* (yr - yP); % Component x
+    rrPxrrQ_y = (xr - xP) .* (zr - zQ) - (xr - xQ) .* (zr - zP); % Component y
+    
+    % Calculate the square of the vector cross product magnitude
+    modulo_rrPxrrQ_2 = (rrPxrrQ_z.^2 + rrPxrrQ_x.^2 + rrPxrrQ_y.^2);
+    
+    % Calculate influence velocities
+    small_value = 1e-12;
+    Velocidad_z = 1 / (4 * pi) * rrPxrrQ_z ./ (modulo_rrPxrrQ_2 + small_value) .* ...
+        (prod_escalar_rPQrrP ./ modulo_rrP - prod_escalar_rPQrrQ ./ modulo_rrQ);
+    Velocidad_y = 1 / (4 * pi) * rrPxrrQ_y ./ (modulo_rrPxrrQ_2 + small_value) .* ...
+        (prod_escalar_rPQrrP ./ modulo_rrP - prod_escalar_rPQrrQ ./ modulo_rrQ);
+    Velocidad_x = 1 / (4 * pi) * rrPxrrQ_x ./ (modulo_rrPxrrQ_2 + small_value) .* ...
+        (prod_escalar_rPQrrP ./ modulo_rrP - prod_escalar_rPQrrQ ./ modulo_rrQ);
 end
--- VLM_Programa_principal.m
+++ VLM_Programa_principal.m
@@ -24,7 +24,7 @@
 % Dependencies:
 % - LeyBiotSavart3D: A custom function for vortex panel integration.
 
-% Last Updated: [Date]
+% Last Updated: June 2020
 
 clear
 
@@ -34,7 +34,7 @@ b = 14;     % Wingspan
 cr = 5;     % Root chord
 E = 0.5;    % Wing taper ratio (0-1)
 psi = 40*pi/180;    % Wing sweep angle (radians)
-alphag = linspace(0,10,1)*pi/180;    % Angle of attack (radians)
+alphag = linspace(0,10,11)*pi/180;    % Angle of attack (radians)
 U_inf = 200;    % Flight velocity (m/s)
 Ny = 20;    % Number of divisions along y-axis
 Nx = 10;    % Number of divisions along x-axis
@@ -74,217 +74,209 @@ for i=1:Ny+1
     c_y(i)=((ct-cr)/b2)*abs(yt(i))+cr;
 end
 
-%% Leading and trailing edge
-
-for i=1:Ny+1
-    x_ataque(i)=tan(psi)*abs(yt(i));
-    x_salida(i)=x_ataque(i)+c_y(i);
-end
+%% Leading and trailing edge calculation
+x_ataque = tan(psi) * abs(yt);
+x_salida = x_ataque + c_y;
 
 %% Primary panel meshing
-for j=1:Ny+1
-    for i=1:Nx+1
-        x_v(i,j)=x_ataque(j)+(x_salida(j)-x_ataque(j))*(i-1)/Nx;
-        y_v(i,j)=yt(j);
-        c_v(i,j)=c_y(j);
+for j = 1:Ny+1
+    for i = 1:Nx+1
+        x_v(i, j) = x_ataque(j) + (x_salida(j) - x_ataque(j)) * (i - 1) / Nx;
+        y_v(i, j) = yt(j);
+        c_v(i, j) = c_y(j);
     end
-    
 end
-    
-%% Vertices de los paneles 1234
+
+%% Vertices of panels 1234 calculation
+G = 1;
+for i = 1:Nx
+    for j = 1:Ny
+        x1(G) = x_v(i, j); y1(G) = y_v(i, j);
+        x2(G) = x_v(i, j + 1); y2(G) = y_v(i, j + 1);
+        x3(G) = x_v(i + 1, j + 1); y3(G) = y_v(i + 1, j + 1);
+        x4(G) = x_v(i + 1, j); y4(G) = y_v(i + 1, j);
+        G = G + 1;
+    end
+end
+
+% Calculate average chord and placement point for each panel
+N = Nx * Ny;
+
+for i = 1:N
+    cm(i) = ((x3(i) + x4(i)) - (x1(i) + x2(i))) / 2;
+    xp(i) = (x2(i) + x1(i)) / 2;
+    xp_2(i) = (x3(i) + x4(i)) / 2;
+    xptotal(i) = xp(i) + 0.75 * (xp_2(i) - xp(i));
+    yp(i) = (y2(i) + y1(i)) / 2;
+    yp_2(i) = (y3(i) + y4(i)) / 2;
+    yptotal(i) = yp(i) + 0.75 * (yp_2(i) - yp(i));
+    cmedia(i) = sqrt((xp(i) - xp_2(i)).^2 + (yp(i) - yp_2(i)).^2);
+    c_yp(i) = ((ct - cr) / b2) * abs(yptotal(i)) + cr;
+end
 
 for i = 1:Nx
     for j = 1:Ny
-  G=(i-1)*Ny+j;
- x1(G) = x_v(i,j); y1(G)=y_v(i,j);
- x2(G) = x_v(i,j+1); y2(G)=y_v(i,j+1); 
- x3(G) = x_v(i+1,j+1); y3(G)=y_v(i+1,j+1); 
- x4(G) = x_v(i+1,j); y4(G)=y_v(i+1,j); 
-     end
-end
-
-%Cuerda media de cada panel y del punto de colocacion para cada panel
-
-N=Nx*Ny;
-
-for i=1:N
-cm(i) = ((x3(i)+x4(i))-(x1(i)+x2(i)))/2;
-xp(i) = (x2(i)+x1(i))./2;
-xp_2(i) = (x3(i)+x4(i))./2;
-xptotal(i) = xp(i)+0.75.*(xp_2(i)-xp(i));
-yp(i) = (y2(i)+y1(i))./2;
-yp_2(i) = (y3(i)+y4(i))./2;
-yptotal(i)=yp(i)+0.75.*(yp_2(i)-yp(i));
-cmedia(i)=sqrt((xp(i)-xp_2(i)).^2+(yp(i)-yp_2(i)).^2);
-c_yp(i)=((ct-cr)/b2)*abs(yptotal(i))+cr;
-end
-
-for i=1:Nx
-    for j=1:Ny
-        G=(i-1)*Ny+j;
-        Xptotal(i,j)=xptotal(G);
-        Yptotal(i,j)=yptotal(G);
-        C_yp(i,j)=c_yp(G);
-        
+        G = (i - 1) * Ny + j;
+        Xptotal(i, j) = xptotal(G);
+        Yptotal(i, j) = yptotal(G);
+        C_yp(i, j) = c_yp(G);
     end
 end
-%Perfil para puntos de colocacion
 
-Xp_total=Xptotal-abs(Yptotal).*tan(psi);
-Xcurvatura=Xp_total./C_yp;
+% Calculate profile for placement points
+Xp_total = Xptotal - abs(Yptotal) .* tan(psi);
+Xcurvatura = Xp_total ./ C_yp;
 
-for j=1:Ny
-    for i=1:Nx
-        if Xcurvatura(i,j) <= p
-           Dx(i,j)=(2*m)/p^2*(p-Xcurvatura(i,j));
-        elseif Xcurvatura(i,j) > p
-           Dx(i,j)=((2*m)/(1-p)^2)*(p-Xcurvatura(i,j));
+for j = 1:Ny
+    for i = 1:Nx
+        if Xcurvatura(i, j) <= p
+            Dx(i, j) = (2 * m) / p^2 * (p - Xcurvatura(i, j));
+        else
+            Dx(i, j) = ((2 * m) / (1 - p)^2) * (p - Xcurvatura(i, j));
         end
-           theta1(i,j)=Dx(i,j);       
+        theta1(i, j) = Dx(i, j);
     end 
 end
 
-for i=1:Nx
-    for j=1:Ny
-        G=(i-1)*Ny+j;
-        thetaC(G)=theta1(i,j);
-    end
-end
-%% Vertices de los paneles ABCD
-
-xAtotal(1:Nx*Ny) = x1(1:Nx*Ny)+(x4(1:Nx*Ny)-x1(1:Nx*Ny))./4; yAtotal(1:Nx*Ny)=y1(1:Nx*Ny); 
-
-xBtotal(1:Nx*Ny) = x2(1:Nx*Ny)+(x3(1:Nx*Ny)-x2(1:Nx*Ny))./4; yBtotal(1:Nx*Ny)=y2(1:Nx*Ny); 
-
-xCtotal(1:Nx*Ny-Ny) = xBtotal((Ny+1):Nx*Ny); yCtotal(1:Nx*Ny)=yBtotal(1:Nx*Ny);
-xCtotal((Nx*Ny-Ny+1):Nx*Ny)=x_salida(2:Ny+1)+1/4;
-
-xDtotal(1:Nx*Ny-Ny) = xAtotal((Ny+1):Nx*Ny); yDtotal(1:Nx*Ny)=yAtotal(1:Nx*Ny);
-xDtotal((Nx*Ny-Ny+1):Nx*Ny)=x_salida(1:Ny)+1/4;
-
-xCtotal(Nx*Ny+1:Nx*Ny+Ny)=100*cr;
-xDtotal(Nx*Ny+1:Nx*Ny+Ny)=100*cr;
-
-xAtotal((Nx*Ny+1:Nx*Ny+Ny))=xDtotal((Nx*Ny-Ny+1):Nx*Ny);
-xBtotal((Nx*Ny+1:Nx*Ny+Ny))=xCtotal((Nx*Ny-Ny+1):Nx*Ny);
-
-yAtotal((Nx*Ny+1:Nx*Ny+Ny))=yAtotal(1:Ny);
-yBtotal((Nx*Ny+1:Nx*Ny+Ny))=yBtotal(1:Ny);
-yCtotal((Nx*Ny+1:Nx*Ny+Ny))=yCtotal(1:Ny);
-yDtotal((Nx*Ny+1:Nx*Ny+Ny))=yDtotal(1:Ny);
-
-%%  Calculos de normales
-
-normal_P_z = -(xptotal(1:Nx*Ny)-x1(1:Nx*Ny)).*(yptotal(1:Nx*Ny)-y2(1:Nx*Ny))...
-+(xptotal(1:Nx*Ny)-x2(1:Nx*Ny)).*(yptotal(1:Nx*Ny)-y1(1:Nx*Ny));
-
-normal_z=normal_P_z./normal_P_z;
-
-%% Calculos del factor de influencia de los segmentos ABCD
-
-N=Nx*Ny;
-Velocidad_total=zeros(1,N);
-
-%Particularizamos en z=0
-z=zeros(1,N);
-
-for i=1:N
-    xcoloc=xptotal(i); ycoloc=yptotal(i); zcoloc=z(i);
-    
-    [Velocidad_z_AB(1:Nx*Ny+Ny)]=LeyBiotSavart3D(xcoloc,ycoloc,zcoloc,xAtotal(1:Nx*Ny+Ny),yAtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny),xBtotal(1:Nx*Ny+Ny),yBtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny));
-    [Velocidad_z_BC(1:Nx*Ny+Ny)]=LeyBiotSavart3D(xcoloc,ycoloc,zcoloc,xBtotal(1:Nx*Ny+Ny),yBtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny),xCtotal(1:Nx*Ny+Ny),yCtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny));
-    [Velocidad_z_CD(1:Nx*Ny+Ny)]=LeyBiotSavart3D(xcoloc,ycoloc,zcoloc,xCtotal(1:Nx*Ny+Ny),yCtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny),xDtotal(1:Nx*Ny+Ny),yDtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny));
-    [Velocidad_z_DA(1:Nx*Ny+Ny)]=LeyBiotSavart3D(xcoloc,ycoloc,zcoloc,xDtotal(1:Nx*Ny+Ny),yDtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny),xAtotal(1:Nx*Ny+Ny),yAtotal(1:Nx*Ny+Ny),zeros(1,Nx*Ny+Ny));
-    
-    Velocidad_z_total=Velocidad_z_AB+Velocidad_z_BC+Velocidad_z_CD+Velocidad_z_DA;
-    
-    VelocidadBCDA=Velocidad_z_BC+Velocidad_z_DA;
-    
-    Velocidad_total(i,1:Nx*Ny+Ny)=Velocidad_z_total.*ones(1,Nx*Ny+Ny);
-    Matriz_bprima1(i,1:Nx*Ny+Ny)=VelocidadBCDA.*ones(1,Nx*Ny+Ny);
-end
-
-Matriz_A_aux=Velocidad_total;
-
-Matriz_A(:,1:Nx*Ny)=Matriz_A_aux(:,1:Nx*Ny);
-Matriz_A(:,Nx*Ny-Ny+1:Nx*Ny)=Matriz_A(:,Nx*Ny-Ny+1:Nx*Ny)+Matriz_A_aux(:,Nx*Ny+1:Nx*Ny+Ny);
-
-
-Matriz_b=-(U_inf*(alpha)*normal_z(1:N))+(U_inf*thetaC.*normal_z(1:N));
-
-Matriz_Gamma=(Matriz_A\Matriz_b'); %Matriz de densidades de circulacion
-
-Matriz_bprima(:,1:Nx*Ny)=Matriz_bprima1(:,1:Nx*Ny);
-
-%% Calculos necesarios para la obtencionn de la circulacion del panel como Matriz (Nx,Ny)
-
-for i=1:Nx
-    for j=1:Ny
-        I=(i-1)*Ny+j;
-        %Area de los paneles
-        Area(i,j)=0.5*(x4(I)-x1(I)+x3(I)-x2(I))*(y2(I)-y1(I));
-        Areavector(I)=Area(i,j);
-        %Matriz de cuerdas medias de los paneles
-        h(i,j)=cm(I);
-        hyp(i,j)=c_yp(I);
-        xcentrogamma(i,j)=1/2*(xAtotal(I)+xBtotal(I));
-        ycentrogamma(i,j)=1/2*(yAtotal(I)+yBtotal(I));
-    end
-end
-
-%Calculos de variables de cada barra
-
-ymed(1:Ny)=0.5*(y_v(1,2:(Ny+1))+y_v(1,1:Ny));
-cy(1:Ny)=0.5*(x_v(Nx+1,2:(Ny+1))+x_v(Nx+1,1:Ny)-x_v(1,(2:(Ny+1)))-x_v(1,1:Ny));
-
-xab(1:Ny)=(x_ataque(2:Ny+1)+x_ataque(1:Ny))*0.5;
-xsb(1:Ny)=(x_salida(2:Ny+1)+x_salida(1:Ny))*0.5;
-cb=(xsb-xab);
-
-%% Calculo de las densidades de circulacion
-
-for i=1:Nx
-    for j=1:Ny
-        G=(i-1)*Ny+j;
-        Gamma(i,j)=Matriz_Gamma(G,1);
-    end
-end
-
-for j=1:Ny
-    
-      Gamma_total(1,j)=Gamma(1,j)/h(1,j);
-      
-     for i=2:Nx
-   
-        Gamma_total(i,j)=(Gamma(i,j)-Gamma(i-1,j))/h(i,j);
+for i = 1:Nx
+    for j = 1:Ny
+        G = (i - 1) * Ny + j;
+        thetaC(G) = theta1(i, j);
     end
 end
 
 
-for j=1:Ny
-Circulacion(j)=Gamma_total(1:Nx,j)'*h(1:Nx,j);
+%% Calculate x and y coordinates for A, B, C, D points
+xAtotal = x1 + (x4 - x1) / 4;
+yAtotal = y1;
+
+xBtotal = x2 + (x3 - x2) / 4;
+yBtotal = y2;
+
+xCtotal = xBtotal(Ny+1:Nx*Ny);
+yCtotal = yBtotal;
+
+xCtotal(Nx*Ny-Ny+1:Nx*Ny) = x_salida(2:Ny+1) + 1/4;
+
+xDtotal = xAtotal(Ny+1:Nx*Ny);
+yDtotal = yAtotal;
+
+xDtotal(Nx*Ny-Ny+1:Nx*Ny) = x_salida(1:Ny) + 1/4;
+
+xCtotal(Nx*Ny+1:Nx*Ny+Ny) = 100 * cr;
+xDtotal(Nx*Ny+1:Nx*Ny+Ny) = 100 * cr;
+
+xAtotal(Nx*Ny+1:Nx*Ny+Ny) = xDtotal(Nx*Ny-Ny+1:Nx*Ny);
+xBtotal(Nx*Ny+1:Nx*Ny+Ny) = xCtotal(Nx*Ny-Ny+1:Nx*Ny);
+
+yAtotal(Nx*Ny+1:Nx*Ny+Ny) = yAtotal(1:Ny);
+yBtotal(Nx*Ny+1:Nx*Ny+Ny) = yBtotal(1:Ny);
+yCtotal(Nx*Ny+1:Nx*Ny+Ny) = yCtotal(1:Ny);
+yDtotal(Nx*Ny+1:Nx*Ny+Ny) = yDtotal(1:Ny);
+
+
+%% Calculations of normals
+normal_P_z = ((xptotal(1:Nx*Ny) - x1(1:Nx*Ny)) .* (yptotal(1:Nx*Ny) - y2(1:Nx*Ny))) ...
+           - ((xptotal(1:Nx*Ny) - x2(1:Nx*Ny)) .* (yptotal(1:Nx*Ny) - y1(1:Nx*Ny)));
+
+normal_z = normal_P_z ./ normal_P_z;
+
+%% Calculations of influence factors for segments ABCD
+
+N = Nx * Ny;
+Velocidad_total = zeros(1, N);
+z = zeros(1, N);
+
+for i = 1:N
+    xcoloc = xptotal(i);
+    ycoloc = yptotal(i);
+    zcoloc = z(i);
+    
+    [Velocidad_z_AB(1:Nx*Ny + Ny)] = LeyBiotSavart3D(xcoloc, ycoloc, zcoloc, xAtotal(1:Nx*Ny + Ny), yAtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny), xBtotal(1:Nx*Ny + Ny), yBtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny));
+    [Velocidad_z_BC(1:Nx*Ny + Ny)] = LeyBiotSavart3D(xcoloc, ycoloc, zcoloc, xBtotal(1:Nx*Ny + Ny), yBtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny), xCtotal(1:Nx*Ny + Ny), yCtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny));
+    [Velocidad_z_CD(1:Nx*Ny + Ny)] = LeyBiotSavart3D(xcoloc, ycoloc, zcoloc, xCtotal(1:Nx*Ny + Ny), yCtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny), xDtotal(1:Nx*Ny + Ny), yDtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny));
+    [Velocidad_z_DA(1:Nx*Ny + Ny)] = LeyBiotSavart3D(xcoloc, ycoloc, zcoloc, xDtotal(1:Nx*Ny + Ny), yDtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny), xAtotal(1:Nx*Ny + Ny), yAtotal(1:Nx*Ny + Ny), zeros(1, Nx*Ny + Ny));
+    
+    Velocidad_z_total = Velocidad_z_AB + Velocidad_z_BC + Velocidad_z_CD + Velocidad_z_DA;
+    VelocidadBCDA = Velocidad_z_BC + Velocidad_z_DA;
+    
+    Velocidad_total(i, 1:Nx*Ny + Ny) = Velocidad_z_total .* ones(1, Nx*Ny + Ny);
+    Matriz_bprima1(i, 1:Nx*Ny + Ny) = VelocidadBCDA .* ones(1, Nx*Ny + Ny);
 end
 
 
+Matriz_A_aux = Velocidad_total;
 
-%Bucle para calculo del incD
+Matriz_A(:, 1:Nx*Ny) = Matriz_A_aux(:, 1:Nx*Ny);
+Matriz_A(:, Nx*Ny - Ny + 1:Nx*Ny) = Matriz_A(:, Nx*Ny - Ny + 1:Nx*Ny) + Matriz_A_aux(:, Nx*Ny + 1:Nx*Ny + Ny);
 
-Velocidad_inducida= Matriz_bprima*Matriz_Gamma;
+Matriz_b = -(U_inf * alpha) * normal_z(1:N) + (U_inf * thetaC .* normal_z(1:N));
 
-for i=1:Nx
-    for j=1:Ny
-        I=(i-1)*Ny+j;
- Velocidad_inducidap(i,j)=Velocidad_inducida(I);
-    end
-end
+Matriz_Gamma = Matriz_A \ Matriz_b'; % Circulation density matrix
 
-for i=1:Nx
-    for j=1:Ny
+Matriz_bprima(:, 1:Nx*Ny) = Matriz_bprima1(:, 1:Nx*Ny);
+
+for i = 1:Nx
+    for j = 1:Ny
+        I = (i - 1) * Ny + j;
         
-        incDprima(i,j)=Gamma_total(i,j)*Velocidad_inducidap(i,j);
+        % Calculate area of panels
+        Area(i, j) = 0.5 * (x4(I) - x1(I) + x3(I) - x2(I)) * (y2(I) - y1(I));
+        Areavector(I) = Area(i, j);
+        
+        % Calculate average chord and related quantities
+        h(i, j) = cm(I);
+        hyp(i, j) = c_yp(I);
+        xcentrogamma(i, j) = 0.5 * (xAtotal(I) + xBtotal(I));
+        ycentrogamma(i, j) = 0.5 * (yAtotal(I) + yBtotal(I));
     end
 end
 
+% Calculate variables for each bar
+ymed(1:Ny) = 0.5 * (y_v(1, 2:(Ny+1)) + y_v(1, 1:Ny));
+cy(1:Ny) = 0.5 * (x_v(Nx+1, 2:(Ny+1)) + x_v(Nx+1, 1:Ny) - x_v(1, (2:(Ny+1))) - x_v(1, 1:Ny));
+
+xab(1:Ny) = 0.5 * (x_ataque(2:Ny+1) + x_ataque(1:Ny));
+xsb(1:Ny) = 0.5 * (x_salida(2:Ny+1) + x_salida(1:Ny));
+cb = xsb - xab;
+
+
+% Calculate circulation densities
+for i = 1:Nx
+    for j = 1:Ny
+        G = (i - 1) * Ny + j;
+        Gamma(i, j) = Matriz_Gamma(G, 1);
+    end
+end
+
+for j = 1:Ny
+    Gamma_total(1, j) = Gamma(1, j) / h(1, j);
+    
+    for i = 2:Nx
+        Gamma_total(i, j) = (Gamma(i, j) - Gamma(i - 1, j)) / h(i, j);
+    end
+end
+
+for j = 1:Ny
+    Circulacion(j) = Gamma_total(1:Nx, j)' * h(1:Nx, j);
+end
+
+% Calculate induced velocities
+Velocidad_inducida = Matriz_bprima * Matriz_Gamma;
+
+for i = 1:Nx
+    for j = 1:Ny
+        I = (i - 1) * Ny + j;
+        Velocidad_inducidap(i, j) = Velocidad_inducida(I);
+    end
+end
+
+% Calculate incDprima
+for i = 1:Nx
+    for j = 1:Ny
+        incDprima(i, j) = Gamma_total(i, j) * Velocidad_inducidap(i, j);
+    end
+end
 
 
 %% Calculos de la sustentacion
@@ -362,114 +354,113 @@ grid on
     end  
    % end
 
+% Define common style for all plots
+lineStyle = '-o';
+legendLocation = 'Best';
+gridStyle = 'on';
+
+% Plot CM vs Cl
 figure(1)
-plot(CM,Cl,'-o')
-title('	Cl vs CM')
-hold on
-xlabel('CM'),ylabel('Cl')
-grid on
+plot(CM, Cl, lineStyle)
+title('Cl vs CM')
+xlabel('CM')
+ylabel('Cl')
+grid(gridStyle)
+legend('Cl vs CM', 'Location', legendLocation)
 
+% Plot Cl vs Cd
 figure(2)
-plot(Cd,Cl,'-o')
-title('	Cl vs Cd')
-hold on
-xlabel('Cd'),ylabel('Cl')
-grid on
+plot(Cd, Cl, lineStyle)
+title('Cl vs Cd')
+xlabel('Cd')
+ylabel('Cl')
+grid(gridStyle)
+legend('Cl vs Cd', 'Location', legendLocation)
 
-
+% Plot Cl vs Angle of Attack
 figure(3)
-plot(alphag*180/pi,Cl,'-o')
-title('	Cl vs Angulo de ataque')
-hold on
-xlabel('\alpha(^o)'),ylabel('Cl')
-grid on
+plot(alphag * 180 / pi, Cl, lineStyle)
+title('Cl vs Angle of Attack')
+xlabel('\alpha (^o)')
+ylabel('Cl')
+grid(gridStyle)
+legend('Cl vs Angle of Attack', 'Location', legendLocation)
 
-%figure(4)
-%plot(psig*180/pi,Cl,'-o')
-%title('	Cl vs Angulo de flecha')
-%hold on
-%xlabel('\psi(^o)'),ylabel('Cl')
-%grid on
-
+% Plot CM vs Angle of Attack
 figure(5)
-plot(alphag*180/pi,CM,'-o')
-title('	CM vs Angulo de ataque')
-hold on
-xlabel('\alpha(^o)'),ylabel('CM')
-grid on
+plot(alphag * 180 / pi, CM, lineStyle)
+title('CM vs Angle of Attack')
+xlabel('\alpha (^o)')
+ylabel('CM')
+grid(gridStyle)
+legend('CM vs Angle of Attack', 'Location', legendLocation)
 
-%figure(6)
-%plot(psig*180/pi,CM,'-o')
-%title('	CM vs Angulo de flecha')
-%hold on
-%xlabel('\psi(^o)'),ylabel('CM')
-%grid on
-
+% Plot Cd vs Angle of Attack
 figure(7)
-plot(alphag*180/pi,Cd,'-o')
-title('	Cd vs Angulo de ataque')
-hold on
-xlabel('\alpha(^o)'),ylabel('Cd')
-grid on
+plot(alphag * 180 / pi, Cd, lineStyle)
+title('Cd vs Angle of Attack')
+xlabel('\alpha (^o)')
+ylabel('Cd')
+grid(gridStyle)
+legend('Cd vs Angle of Attack', 'Location', legendLocation)
 
-%figure(8)
-%plot(psig*180/pi,Cd,'-o')
-%title('	Cd vs Angulo de flecha')
-%hold on
-%xlabel('\psi(^o)'),ylabel('Cd')
-%grid on
-
+% Plot Moments of Pitch vs Chord
 figure(9)
-plot(xAtaque/cr,Mya)
-%legend({'Momentos de cabeceo'},'Location','Northeast')
-title('	Momentos de cabeceo')
-hold on
-legend ('\alpha = 0^o','\alpha = 2^o','\alpha = 4^o','\alpha = 5^o','\alpha = 6^o','\alpha = 8^o','\alpha = 10^o')
-xlabel('Borde de ataque'),ylabel('Momento de cabeceo')
-grid on
+plot(xAtaque / cr, Mya, lineStyle)
+title('Moments of Pitch')
+xlabel('Normalized Chord Length (x/c)')
+ylabel('Moment of Pitch')
+grid(gridStyle)
+legend('\alpha = 0^o', '\alpha = 2^o', '\alpha = 4^o', '\alpha = 5^o', '\alpha = 6^o', '\alpha = 8^o', '\alpha = 10^o', 'Location', legendLocation)
 
+% Plot Lift Coefficient Distribution
 figure(10)
-plot(ymed,distr_Cl)
-legend({'Cl'},'Location','Southeast')
-title('Coeficiente de sustentacion a lo largo de la envergadura')
-hold on
-xlabel('Envergadura'),ylabel('Coeficiente de sustentacion')
-legend ('\alpha = 0^o','\alpha = 2^o','\alpha = 4^o','\alpha = 5^o','\alpha = 6^o','\alpha = 8^o','\alpha = 10^o')
-grid on
+plot(ymed, distr_Cl, lineStyle)
+title('Lift Coefficient Distribution')
+xlabel('Spanwise Position (y/b)')
+ylabel('Lift Coefficient (Cl)')
+grid(gridStyle)
+legend('\alpha = 0^o', '\alpha = 2^o', '\alpha = 4^o', '\alpha = 5^o', '\alpha = 6^o', '\alpha = 8^o', '\alpha = 10^o', 'Location', legendLocation)
 
-  
+% Plot Lift Distribution
 figure(11)
+plot(ymed, distr_L, lineStyle)
+title('Lift Distribution Along Span')
+xlabel('Spanwise Position (y/b)')
+ylabel('Lift (L)')
+grid(gridStyle)
+legend('Lift Distribution', 'Location', legendLocation)
 
-plot(ymed,distr_L)
-legend({'L'},'Location','Southeast')
-title('Sustentacion a lo largo de la envergadura')
-hold on
-xlabel('Envergadura'),ylabel('Sustentacion')
-grid on
      
     
- figure(12)
-mesh(xptotalp/cr,yptotalp/b2,Cp)
-legend({'Cp'},'Location','Southeast')
-title('Distribucion de Cp a lo largo del ala')
-xlabel('x'),ylabel('y'),zlabel('z')
+% Plot for Cp Distribution as Mesh
+figure(12)
+mesh(xptotalp / cr, yptotalp / b2, Cp)
+title('Cp Distribution Along the Wing')
+xlabel('x/c')
+ylabel('y/b')
+zlabel('Cp')
+legend('Cp', 'Location', 'southeast')
 grid on
 
+% Plot for Cp Distribution as Individual Surfaces
 figure(13)
-
 rotate3d on
 colormap(hot);
 
-for i=1:Nx
-    for j=1:Ny
-h1=surf(x_v(i:i+1,j:j+1),y_v(i:i+1,j:j+1),zeros(size(x_v(i:i+1,j:j+1))),Cp(i,j));
-c=colorbar;
-c.Label.String='Cp';
-title('Distribucion de Cp')
-xlabel('x'),ylabel('y'),zlabel('z')
-grid on
-axis equal
-hold on
+for i = 1:Nx
+    for j = 1:Ny
+        h1 = surf(x_v(i:i+1, j:j+1), y_v(i:i+1, j:j+1), zeros(size(x_v(i:i+1, j:j+1))), Cp(i, j));
+        c = colorbar;
+        c.Label.String = 'Cp';
+        title('Cp Distribution')
+        xlabel('x')
+        ylabel('y')
+        zlabel('z')
+        grid on
+        axis equal
+        hold on
     end
-    hold on
 end
+
+hold off
